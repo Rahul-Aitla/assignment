d4d37968f9f990c960fef2e3a3b10ead9420236e
Rahul-Aitla <aitlarahul@gmail.com>
2025-11-22 00:13:05 +0530

    deploy

--- health-desk-clinic/src/components/Testimonials.jsx
+++ health-desk-clinic/src/components/Testimonials.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Testimonials.css';
 
+import testo1 from '../assets/images/testo1.png';
+import testo2 from '../assets/images/testo2.png';
+import testo3 from '../assets/images/test03.png';
+
 const Testimonials = () => {
     const testimonials = [
         {
@@ -8,21 +12,21 @@ const Testimonials = () => {
             product: 'TestoBites',
             rating: 5,
             text: 'TestoBites has been a game changer when it comes to my stamina and energy levels. I can truly feel a difference in my sex drive and virility as well. Highly recommended.',
-            image: null // Placeholder
+            image: testo1
         },
         {
             name: 'Jamie Fields',
             product: 'Vita Renew',
             rating: 5,
             text: 'My pain therapist recommended this product to me and I was surprised by how well it worked. I can see a marked improvement in my Skin Care.',
-            image: null // Placeholder
+            image: testo2
         },
         {
             name: 'Anonymous',
             product: 'Nerve Freedom',
             rating: 5,
             text: 'I have been an RN for 20 years and love helping people find solutions to their health issues. NF Product is the best natural product for neuropathy pain hands down.',
-            image: null // Placeholder
+            image: testo3
         }
     ];
 
@@ -34,17 +38,18 @@ const Testimonials = () => {
                     {testimonials.map((item, index) => (
                         <div key={index} className="testimonial-card">
                             <div className="testimonial-product-image">
-                                {/* Placeholder */}
-                                <div className="t-image-placeholder">Product</div>
+                                <img src={item.image} alt={item.product} />
                             </div>
-                            <h3 className="testimonial-product-name">{item.product}</h3>
-                            <div className="testimonial-rating">
-                                {[...Array(item.rating)].map((_, i) => (
-                                    <span key={i} className="star">★</span>
-                                ))}
+                            <div className="testimonial-content">
+                                <h3 className="testimonial-product-name">{item.product}</h3>
+                                <div className="testimonial-rating">
+                                    {[...Array(item.rating)].map((_, i) => (
+                                        <span key={i} className="star">★</span>
+                                    ))}
+                                </div>
+                                <p className="testimonial-text">{item.text}</p>
+                                <p className="testimonial-author">- {item.name}</p>
                             </div>
-                            <p className="testimonial-text">"{item.text}"</p>
-                            <p className="testimonial-author">- {item.name}</p>
                         </div>
                     ))}
                 </div>
